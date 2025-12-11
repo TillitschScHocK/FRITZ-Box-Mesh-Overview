eec93575e935af493e34fe2473ab069ce7a16be0
Till <94850668+TillitschScHocK@users.noreply.github.com>
2025-12-11 18:28:56 +0100

    Fix: run.sh - Bessere Fehlerbehandlung und Logging

--- fritzmesh_addon/run.sh
+++ fritzmesh_addon/run.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/with-contenv bashio
 
-# Config-Werte lesen
+set -e
+
+# Home Assistant Addon Config auslesen
 FRITZ_HOST=$(bashio::config 'fritz_host')
 FRITZ_PASS=$(bashio::config 'fritz_pass')
 FRITZ_USER=$(bashio::config 'fritz_user')
 REFRESH_RATE=$(bashio::config 'refresh_rate')
 
-# Als Umgebungsvariablen exportieren
+# Umgebungsvariablen exportieren
 export FRITZ_HOST
 export FRITZ_PASS
 export FRITZ_USER
 export REFRESH_RATE
 
-bashio::log.info "Starte Fritz!Box Mesh Overview..."
-bashio::log.info "Fritz!Box Host: ${FRITZ_HOST}"
+bashio::log.info "========================================="
+bashio::log.info "Fritz!Box Mesh Overview v2.0.2"
+bashio::log.info "========================================="
+bashio::log.info "Host: ${FRITZ_HOST}"
+bashio::log.info "Benutzer: ${FRITZ_USER}"
+bashio::log.info "Refresh: ${REFRESH_RATE}s"
+bashio::log.info "Starte Python-Anwendung..."
+bashio::log.info "========================================="
 
 # Python-App starten
 exec python3 /app/main.py
